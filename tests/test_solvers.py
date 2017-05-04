2b856ed66257aab3764f4efbc7737cd96dbee231
qbit- <rss3@rice.edu>
2017-05-04 17:49:58 -0500

    working root solver

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -31,10 +31,11 @@ class TestCCSolversModule(unittest.TestCase):
         self.assertEqual(converged, True)
         self.assertEqual(np.allclose(energy, -0.2133432609672395, 1e-5), True)
 
-        converged, energy, _ = residual_diis_solver(cc, amps, lam=3, conv_tol_energy=1e-10)
+        converged, energy, _ = residual_diis_solver(
+            cc, amps, lam=3, conv_tol_energy=1e-10)
         self.assertEqual(converged, True)
         self.assertEqual(np.allclose(energy, -0.2133432609672395, 1e-5), True)
-        
+
     def test_classic_solver(self):
         from tcc.cc_solvers import classic_solver
         from tcc.rccsd import RCCSD
@@ -44,9 +45,19 @@ class TestCCSolversModule(unittest.TestCase):
         self.assertEqual(converged, True)
         self.assertEqual(np.allclose(energy, -0.2133432609672395, 1e-5), True)
 
-        converged, energy, _ = classic_solver(cc, amps, lam=3, conv_tol_energy=1e-10)
+        converged, energy, _ = classic_solver(
+            cc, amps, lam=3, conv_tol_energy=1e-10)
         self.assertEqual(converged, True)
         self.assertEqual(np.allclose(energy, -0.2133432609672395, 1e-5), True)
-        
+
+    def test_root_solver(self):
+        from tcc.cc_solvers import root_solver
+        from tcc.rccsd import RCCSD
+        cc = RCCSD(self.rhf)
+
+        converged, energy, amps = root_solver(cc)
+        self.assertEqual(converged, True)
+        self.assertEqual(np.allclose(energy, -0.2133432609672395, 1e-5), True)
+
 if __name__ == '__main__':
     unittest.main()
