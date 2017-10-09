f18af829fd0aa2f9339503c445ff6077a0f344b0
qbit- <rss3@rice.edu>
2017-10-09 15:22:42 -0500

    rewritten iteraction passes tests

--- interaction.py
+++ interaction.py
@@ -15,19 +15,19 @@ def ref_ndarray(a):
     return np.array(a, copy=False, order='C')
 
 
-
 def _calculate_noncanonical_fock(scf, mo_coeff, mo_occ):
     """
     Calculates Fock matrix in non-canonical basis
     """
     dm = scf.make_rdm1(mo_coeff, mo_occ)
     fockao = scf.get_hcore() + scf.get_veff(scf.mol, dm)
-    if len(fockao.shape) == 2: # spinless orbitals
+    if len(fockao.shape) == 2:  # spinless orbitals
         return reduce(np.dot, (mo_coeff.T, fockao, mo_coeff))
-    elif len(fockao.shape) == 3: # uhf orbitals
-        return np.array([reduce(np.dot, (mo_coeff[0].T, fockao[0], mo_coeff[0])),
-                         reduce(np.dot, (mo_coeff[1].T, fockao[1], mo_coeff[1]))])
-
+    elif len(fockao.shape) == 3:  # uhf orbitals
+        return np.array([reduce(
+            np.dot, (mo_coeff[0].T, fockao[0], mo_coeff[0])),
+            reduce(
+            np.dot, (mo_coeff[1].T, fockao[1], mo_coeff[1]))])
 
 
 def _assemble_spinless_fock(cc, mos=None):
@@ -42,10 +42,9 @@ def _assemble_spinless_fock(cc, mos=None):
     f = Tensors(oo=ref_ndarray(fock[:nocc, :nocc]),
                 ov=ref_ndarray(fock[:nocc, nocc:]),
                 vv=ref_ndarray(fock[nocc:, nocc:])
-    )
+                )
 
     return f
-
 
 
 def _assemble_uhf_fock(cc, mos=None):
@@ -53,22 +52,21 @@ def _assemble_uhf_fock(cc, mos=None):
     if mos is None:  # Assume canonical orbitals
         mos = cc.mos
         fock = np.array([np.diag(cc.mos.a.mo_energies),
-                np.diag(cc.mos.b.mo_energies)])
+                         np.diag(cc.mos.b.mo_energies)])
     else:  # If mo_coeff is not canonical orbitals
         fock = _calculate_noncanonical_fock(
             cc._scf,
-            np.array((mos.a.mo_coeff,mos.b.mo_coeff)),
+            np.array((mos.a.mo_coeff, mos.b.mo_coeff)),
             np.array((mos.a.mo_occ, mos.b.mos_occ)))
     nocca = mos.a.nocc
     noccb = mos.b.nocc
-    return  Tensors(a=Tensors(oo=ref_ndarray(fock[0][:nocca, :nocca]),
-                          ov=ref_ndarray(fock[0][:nocca, nocca:]),
-                          vv=ref_ndarray(fock[0][nocca:, nocca:])),
-                b=Tensors(oo=ref_ndarray(fock[1][:noccb, :noccb]),
-                          ov=ref_ndarray(fock[1][:noccb, noccb:]),
-                          vv=ref_ndarray(fock[1][noccb:, noccb:])),
-    )
-
+    return Tensors(a=Tensors(oo=ref_ndarray(fock[0][:nocca, :nocca]),
+                             ov=ref_ndarray(fock[0][:nocca, nocca:]),
+                             vv=ref_ndarray(fock[0][nocca:, nocca:])),
+                   b=Tensors(oo=ref_ndarray(fock[1][:noccb, :noccb]),
+                             ov=ref_ndarray(fock[1][:noccb, noccb:]),
+                             vv=ref_ndarray(fock[1][noccb:, noccb:])),
+                   )
 
 
 def _assemble_fock(cc, mos=None):
@@ -77,11 +75,10 @@ def _assemble_fock(cc, mos=None):
         m = mos
     else:
         m = cc.mos
-    if hasattr(m, 'a'): # UHF mos will have spin part attributes
+    if hasattr(m, 'a'):  # UHF mos will have spin part attributes
         return _assemble_uhf_fock(cc, mos)
     else:
         return _assemble_spinless_fock(cc, mos)
-
 
 
 def _transform_aoeri(eri, mosa, mosb=None):
@@ -96,8 +93,8 @@ def _transform_aoeri(eri, mosa, mosb=None):
             [mosa.mo_coeff, mosa.mo_coeff, mosb.mo_coeff, mosb.mo_coeff],
             compact=False)
     # reshape to a 4-index tensor
-    eri = ao2mo.restore(8, eri, nmo)
-    
+    eri = np.reshape(eri, (nmo, nmo, nmo, nmo))
+
     return eri
 
 
@@ -107,13 +104,13 @@ def _extract_blocks_mul(eri, nocc1, nocc2):
     from Mulliken ordered integrals
     """
     return Tensors(oooo=ref_ndarray(eri[:nocc1, :nocc1, :nocc2, :nocc2]),
-                     ooov=ref_ndarray(eri[:nocc1, :nocc1, :nocc2, nocc2:]),
-                     oovv=ref_ndarray(eri[:nocc1, :nocc1, nocc2:, nocc2:]),
-                     ovov=ref_ndarray(eri[:nocc1, nocc1:, :nocc2, nocc2:]),
-                     voov=ref_ndarray(eri[nocc1:, :nocc1, :nocc2, nocc2:]),
-                     ovvv=ref_ndarray(eri[:nocc1, nocc1:, nocc2:, nocc2:]),
-                     vvvv=ref_ndarray(eri[nocc1:, nocc1:, nocc2:, nocc2:])
-    )
+                   ooov=ref_ndarray(eri[:nocc1, :nocc1, :nocc2, nocc2:]),
+                   oovv=ref_ndarray(eri[:nocc1, :nocc1, nocc2:, nocc2:]),
+                   ovov=ref_ndarray(eri[:nocc1, nocc1:, :nocc2, nocc2:]),
+                   voov=ref_ndarray(eri[nocc1:, :nocc1, :nocc2, nocc2:]),
+                   ovvv=ref_ndarray(eri[:nocc1, nocc1:, nocc2:, nocc2:]),
+                   vvvv=ref_ndarray(eri[nocc1:, nocc1:, nocc2:, nocc2:])
+                   )
 
 
 def _extract_blocks_dir(eri, nocc1, nocc2):
@@ -136,23 +133,20 @@ def _extract_blocks_dir(eri, nocc1, nocc2):
                    ovoo=eri[:nocc1, nocc2:, :nocc1, :nocc2],
                    oovo=eri[:nocc1, :nocc2, nocc1:, :nocc2],
                    vvvo=eri[nocc1:, nocc2:, nocc1:, :nocc2]
-    )
+                   )
 
 
-    
 def _assemble_moeri_full_core(scf, mosa, mosb=None, order='mul'):
     """
     Gets AO eris, transorms them to MO bases and returns relevant blocks
     in a selected order
     """
     nocc1 = mosa.nocc
-    nmo = mosa.nmo
-    import pdb
-    pdb.set_trace()
+
     if mosb is None:
         nocc2 = nocc1
     else:
-        nocc2 = mob.nocc
+        nocc2 = mosb.nocc
     eri = _transform_aoeri(scf._eri, mosa, mosb)
 
     if order == 'mul':
@@ -167,13 +161,13 @@ def _assemble_moeri_ri_core(scf, mos):
     and takes relevant blocks
     """
 
-    nocc = mosa.nocc
-    nmo = mosa.nmo
+    nocc = mos.nocc
+    nmo = mos.nmo
 
     naux = scf.with_df.get_naoaux()
     Lpnn = np.empty((naux, nmo, nmo))
 
-    mof = np.asarray(mosa.mo_coeff, order='F')
+    mof = np.asarray(mos.mo_coeff, order='F')
     ijslice = (0, nmo, 0, nmo)
     Lpq = None
     pq = 0
@@ -187,9 +181,9 @@ def _assemble_moeri_ri_core(scf, mos):
     return Tensors(poo=ref_ndarray(Lpnn[:, :nocc, :nocc]),
                    pov=ref_ndarray(Lpnn[:, :nocc, nocc:]),
                    pvv=ref_ndarray(Lpnn[:, nocc:, nocc:]),
-                   pvo=ref_ndarray(Lpnn[:, nocc:, :nocc])                        
-    )
-        
+                   pvo=ref_ndarray(Lpnn[:, nocc:, :nocc])
+                   )
+
 
 def _create_hub_aoeri(u, nao):
     """
@@ -199,7 +193,6 @@ def _create_hub_aoeri(u, nao):
     eri = np.zeros((nao, nao, nao, nao))
     for i in range(nao):
         eri[i, i, i, i] = u
-    eri = ao2mo.restore(8, eri, nao)
 
     return eri
 
@@ -209,18 +202,16 @@ def _assemble_moeri_full_core_hub(scf, mosa, mosb=None, order='mul'):
     Builds on-site eris, transforms them to MO basis,
     returns relevant blocks
     """
-    nocc = mosa.nocc
-    nmo = mosa.nmo
-
+    nocc1 = mosa.nocc
     nao = mosa.mo_coeff.shape[0]
 
     u = scf._hubbard_interaction
     eri = _create_hub_aoeri(u, nao)
-    
+
     if mosb is None:
         nocc2 = nocc1
     else:
-        nocc2 = mob.nocc
+        nocc2 = mosb.nocc
 
     eri = _transform_aoeri(eri, mosa, mosb)
 
@@ -242,7 +233,7 @@ def _assemble_moeri_ri_core_hub(scf, mosa, mosb=None):
 
     from tcc.utils import khatrirao
     from math import sqrt
-    
+
     # Build RI integrals analytically
     u = scf._hubbard_interaction
     if u < 0:
@@ -256,12 +247,11 @@ def _assemble_moeri_ri_core_hub(scf, mosa, mosb=None):
              mosa.mo_coeff.T)
         )), (nmo, nmo, nmo)
     )
-    
+
     return Tensors(poo=ref_ndarray(Lpnn[:, :nocc, :nocc]),
                    pov=ref_ndarray(Lpnn[:, :nocc, nocc:]),
                    pvv=ref_ndarray(Lpnn[:, nocc:, nocc:]),
                    pvo=ref_ndarray(Lpnn[:, nocc:, :nocc]))
-
 
 
 def _assemble_moeri_thc_core_hub(scf, mosa, mosb=None):
@@ -276,16 +266,15 @@ def _assemble_moeri_thc_core_hub(scf, mosa, mosb=None):
     # Build THC integrals analytically
 
     u = scf._hubbard_interaction
-    CT = (mosa.mo_coeff) # Why was it done this way?
-    
+    CT = (mosa.mo_coeff)  # Why was it done this way?
+
     return Tensors(x1=Tensors(CT[:nocc, :], CT[nocc:, :]),
-                     x2=Tensors(CT[:nocc, :], CT[nocc:, :]),
-                     x3=Tensors(CT[:nocc, :], CT[nocc:, :]),
-                     x4=Tensors(CT[:nocc, :], CT[nocc:, :]),
-                    x5=u * np.eye(nao))
+                   x2=Tensors(CT[:nocc, :], CT[nocc:, :]),
+                   x3=Tensors(CT[:nocc, :], CT[nocc:, :]),
+                   x4=Tensors(CT[:nocc, :], CT[nocc:, :]),
+                   x5=u * np.eye(nao))
 
 
-    
 class HAM_SPINLESS_FULL_CORE_MUL:
     """
     Mulliken ordered hamiltonian
@@ -310,7 +299,6 @@ class HAM_SPINLESS_FULL_CORE_MUL:
             raise ValueError('SCF object did not supply AO integrals')
 
         log.timer('CCSD integral transformation', *cput0)
-
 
 
 class HAM_SPINLESS_FULL_CORE_DIR:
@@ -345,7 +333,6 @@ class HAM_SPINLESS_FULL_CORE_DIR:
         log.timer('CCSD integral transformation', *cput0)
 
 
-        
 class HAM_SPINLESS_RI_CORE:
 
     def __init__(self, cc, mos=None):
@@ -362,13 +349,12 @@ class HAM_SPINLESS_RI_CORE:
             self.mos = mos
 
         if hasattr(cc._scf, 'with_df') and cc._scf.with_df:
-            
+
             self.l = _assemble_moeri_ri_core(cc._scf, self.mos)
         else:
             raise ValueError('SCF object did not supply DF AO integrals')
 
         log.timer('CCSD integral transformation', *cput0)
-
 
 
 class HAM_SPINLESS_FULL_CORE_HUBBARD:
@@ -392,12 +378,12 @@ class HAM_SPINLESS_FULL_CORE_HUBBARD:
         # Build Hubbard integrals analytically
 
         if hasattr(cc._scf, '_hubbard_interaction'):
-            self.v = _assemble_moeri_full_core_hub(cc._scf, self.mos, order=order)
+            self.v = _assemble_moeri_full_core_hub(
+                cc._scf, self.mos, order=order)
         else:
             raise ValueError('SCF object did not supply Hubbard interaction')
 
         log.timer('CCSD integral transformation', *cput0)
-
 
 
 class HAM_SPINLESS_RI_CORE_HUBBARD:
@@ -419,12 +405,11 @@ class HAM_SPINLESS_RI_CORE_HUBBARD:
         self.f = _assemble_fock(cc, mos)
 
         if hasattr(cc._scf, '_hubbard_interaction'):
-            self.l = _assemble_moeri_ri_core_hub(cc._scf, self.mos) 
+            self.l = _assemble_moeri_ri_core_hub(cc._scf, self.mos)
         else:
             raise ValueError('SCF object did not supply Hubbard interaction')
 
         log.timer('CCSD integral transformation', *cput0)
-
 
 
 class HAM_SPINLESS_THC_CORE_HUBBARD:
@@ -454,7 +439,6 @@ class HAM_SPINLESS_THC_CORE_HUBBARD:
         log.timer('CCSD integral transformation', *cput0)
 
 
-
 class _HAM_SPINLESS_FULL_CORE_DIR_MATFILE:
     """
     Dirac ordered hamiltonian
@@ -467,7 +451,6 @@ class _HAM_SPINLESS_FULL_CORE_DIR_MATFILE:
         # Get sizes
         self.mos = cc.mos
         nocc = self.mos.nocc
-        nmo = self.mos.nmo
 
         from scipy.io import loadmat
         from os.path import isfile
@@ -479,7 +462,7 @@ class _HAM_SPINLESS_FULL_CORE_DIR_MATFILE:
             self.f = Tensors(oo=fock[:nocc, :nocc],
                              ov=fock[:nocc, nocc:],
                              vv=fock[nocc:, nocc:]
-            )
+                             )
 
         if (isfile(filename)):
 
@@ -491,7 +474,6 @@ class _HAM_SPINLESS_FULL_CORE_DIR_MATFILE:
             raise ValueError('File not found: {}'.format(filename))
 
         log.timer('CCSD integral transformation', *cput0)
-
 
 
 class _HAM_SPINLESS_THC_CORE_MATFILE:
@@ -510,7 +492,6 @@ class _HAM_SPINLESS_THC_CORE_MATFILE:
         # Get sizes
         self.mos = cc.mos
         nocc = self.mos.nocc
-        nmo = self.mos.nmo
 
         from scipy.io import loadmat
         from os.path import isfile
@@ -525,7 +506,7 @@ class _HAM_SPINLESS_THC_CORE_MATFILE:
             self.f = Tensors(oo=fock[:nocc, :nocc],
                              ov=fock[:nocc, nocc:],
                              vv=fock[nocc:, nocc:]
-            )
+                             )
 
         if (isfile(filename)):
             wf1 = loadmat(filename, variable_names=('WFmo'),
@@ -540,13 +521,13 @@ class _HAM_SPINLESS_THC_CORE_MATFILE:
                              x4=Tensors(o=wf1[3][:nocc, :],
                                         v=wf1[3][nocc:, :]),
                              x5=wf1[4]
-            )
+                             )
         else:
             raise ValueError('File not found: {}'.format(filename))
 
         log.timer('CCSD integral transformation', *cput0)
 
-        
+
 class HAM_UHF_FULL_CORE_MUL:
     """
     Mulliken ordered hamiltonian
@@ -566,9 +547,12 @@ class HAM_UHF_FULL_CORE_MUL:
         self.f = _assemble_fock(cc, mos)
 
         if (cc._scf._eri is not None):
-            self.v.aaaa = _assemble_moeri_full_core(cc._scf, self.mos.a, order='mul')
-            self.v.bbbb = _assemble_moeri_full_core(cc._scf, self.mos.b, order='mul')
-            self.v.aabb = _assemble_moeri_full_core(cc._scf, self.mos.a, self.mos.b, order='mul')
+            self.v.aaaa = _assemble_moeri_full_core(
+                cc._scf, self.mos.a, order='mul')
+            self.v.bbbb = _assemble_moeri_full_core(
+                cc._scf, self.mos.b, order='mul')
+            self.v.aabb = _assemble_moeri_full_core(
+                cc._scf, self.mos.a, self.mos.b, order='mul')
         else:
             raise ValueError('SCF object did not supply AO integrals')
 
@@ -594,9 +578,12 @@ class HAM_UHF_FULL_CORE_DIR:
         self.f = _assemble_fock(cc, mos)
 
         if (cc._scf._eri is not None):
-            self.v.aaaa = _assemble_moeri_full_core(cc._scf, self.mos.a, order='dir')
-            self.v.bbbb = _assemble_moeri_full_core(cc._scf, self.mos.b, order='dir')
-            self.v.abab = _assemble_moeri_full_core(cc._scf, self.mos.a, self.mos.b, order='dir')
+            self.v.aaaa = _assemble_moeri_full_core(
+                cc._scf, self.mos.a, order='dir')
+            self.v.bbbb = _assemble_moeri_full_core(
+                cc._scf, self.mos.b, order='dir')
+            self.v.abab = _assemble_moeri_full_core(
+                cc._scf, self.mos.a, self.mos.b, order='dir')
         else:
             raise ValueError('SCF object did not supply AO integrals')
 
@@ -622,7 +609,7 @@ class HAM_UHF_RI_CORE:
         self.f = _assemble_fock(cc, mos)
 
         if (cc._scf._eri is not None):
-            
+
             ints_a = _assemble_moeri_ri_core(cc._scf, self.mos.a)
             ints_b = _assemble_moeri_ri_core(cc._scf, self.mos.b)
             self.l = Tensors(a=ints_a, b=ints_b)
@@ -630,5 +617,3 @@ class HAM_UHF_RI_CORE:
             raise ValueError('SCF object did not supply AO integrals')
 
         log.timer('CCSD integral transformation', *cput0)
-
-
